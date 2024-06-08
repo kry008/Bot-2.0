464ff0106ba038caee425fa5a014eedec3fa6a80
kry008 <git@mail.kry008.xyz>
2024-06-08 10:21:24 +0200

    Support bot command

--- commands/supportBot.js
+++ commands/supportBot.js
@@ -1,121 +1,118 @@
-const { EmbedBuilder } = require('discord.js');
-function randomColor() {
-    return Math.floor(Math.random()*16777215).toString(16);
-}
-
 module.exports = {
     name: 'supportbot',
     description: 'How to support bot development?',
     help: 'How to support bot development?',
     options: [],
-    slash: false,
-    text: false,
+    slash: true,
+    text: true,
     admin: false,
     requireKick: false,
     requireBan: false,
     canBeUsedInDm: true,
     contexts: ['GUILD_TEXT', 'GUILD_VOICE', 'DM'],
     integration_types: [0,1],
-    execute(message, args) {
-        const msg = {
-            color: 0x0099ff,
-            title: 'Some title',
-            url: 'https://discord.js.org',
+    execute: async (message, args) => {
+        message.channel.send({ embeds: [{
+            color: 3447003,
             author: {
-                name: 'Some name',
-                icon_url: 'https://i.imgur.com/AfFp7pu.png',
-                url: 'https://discord.js.org',
+              name: "kry008",
+              icon_url: "https://raw.githubusercontent.com/kry008/kry008.xyz/main/images/logo.webp"
             },
-            description: 'Some description here',
             thumbnail: {
-                url: 'https://i.imgur.com/AfFp7pu.png',
+              url: "https://cdn.discordapp.com/avatars/883390927383724112/1fdd55ef73a631edfd680964a28ca280.webp"
             },
-            fields: [
-                {
-                    name: 'Regular field title',
-                    value: 'Some value here',
-                },
-                {
-                    name: '\u200b',
-                    value: '\u200b',
-                    inline: false,
-                },
-                {
-                    name: 'Inline field title',
-                    value: 'Some value here',
-                    inline: true,
-                },
-                {
-                    name: 'Inline field title',
-                    value: 'Some value here',
-                    inline: true,
-                },
-                {
-                    name: 'Inline field title',
-                    value: 'Some value here',
-                    inline: true,
-                },
-            ],
-            image: {
-                url: 'https://i.imgur.com/AfFp7pu.png',
+            //image: {
+            //  url: "http://i.imgur.com/yVpymuV.png"
+            //},
+            title: "Multitool simple bot",
+            url: "https://kry008.xyz/bot",
+            description: "Bot made with passion.",
+            fields: [{
+              name: "To support my projects",
+              value: "",
+              inline: false
             },
-            timestamp: new Date().toISOString(),
+            {
+              name: "Patronite.pl",
+              value: "[**Patronite**](https://patronite.pl/kry008) - You can support me on Patronite.",
+              inline: true
+            },
+            {
+              name: "Ko-fi",
+              value: "[**Ko-fi**](https://ko-fi.com/kry008) - You can buy me a coffee (payment not implemented yet).",
+              inline: true
+            },
+            {
+              name: "New ides for bot?",
+              value: "You can send me on bot [**website**](https://kry008.xyz/bot).",
+              inline: false
+            },
+            {
+              name: "To see other projects",
+              value: "[**Private git**](https://git.kry008.xyz) or [Github](https://github.com/kry008)",
+              inline: false
+            },
+            {
+              name: "\u200b",
+              value:"\u200b"
+            }],
+            timestamp: new Date(),
             footer: {
-                text: 'Some footer text here',
-                icon_url: 'https://i.imgur.com/AfFp7pu.png',
-            },
-        };
-        message.reply({ embeds: msg });
+              icon_url: "https://cdn.discordapp.com/avatars/883390927383724112/1fdd55ef73a631edfd680964a28ca280.webp",
+              text: "Mini bot says hello!"
+            }
+          }]});
     },
-    executeSlash(interaction) {
-        const msg = {
-            color: 0x0099ff,
-            title: 'Some title',
-            url: 'https://discord.js.org',
+    executeSlash: async interaction => {
+        interaction.reply({ embeds: [{
+            color: 3447003,
             author: {
-                name: 'Some name',
-                icon_url: 'https://i.imgur.com/AfFp7pu.png',
-                url: 'https://discord.js.org',
-            },
-            description: 'Some description here',
-            thumbnail: {
-                url: 'https://i.imgur.com/AfFp7pu.png',
-            },
-            fields: [
-                {
-                    name: 'Regular field title',
-                    value: 'Some value here',
-                },
-                {
-                    name: '\u200b',
-                    value: '\u200b',
-                    inline: false,
-                },
-                {
-                    name: 'Inline field title',
-                    value: 'Some value here',
-                    inline: true,
-                },
-                {
-                    name: 'Inline field title',
-                    value: 'Some value here',
-                    inline: true,
-                },
-                {
-                    name: 'Inline field title',
-                    value: 'Some value here',
-                    inline: true,
-                },
-            ],
-            image: {
-                url: 'https://i.imgur.com/AfFp7pu.png',
-            },
-            timestamp: new Date().toISOString(),
-            footer: {
-                text: 'Some footer text here',
-                icon_url: 'https://i.imgur.com/AfFp7pu.png',
-            },
-        };
-        interaction.reply({ embeds: msg });
+                name: "kry008",
+                icon_url: "https://raw.githubusercontent.com/kry008/kry008.xyz/main/images/logo.webp"
+              },
+              thumbnail: {
+                url: "https://cdn.discordapp.com/avatars/883390927383724112/1fdd55ef73a631edfd680964a28ca280.webp"
+              },
+              //image: {
+              //  url: "http://i.imgur.com/yVpymuV.png"
+              //},
+              title: "Multitool simple bot",
+              url: "https://kry008.xyz/bot",
+              description: "Bot made with passion.",
+              fields: [{
+                name: "To support my projects",
+                value: "",
+                inline: false
+              },
+              {
+                name: "Patronite.pl",
+                value: "[**Patronite**](https://patronite.pl/kry008) - You can support me on Patronite.",
+                inline: true
+              },
+              {
+                name: "Ko-fi",
+                value: "[**Ko-fi**](https://ko-fi.com/kry008) - You can buy me a coffee (payment not implemented yet).",
+                inline: true
+              },
+              {
+                name: "New ides for bot?",
+                value: "You can send me on bot [**website**](https://kry008.xyz/bot).",
+                inline: false
+              },
+              {
+                name: "To see other projects",
+                value: "[**Private git**](https://git.kry008.xyz) or [Github](https://github.com/kry008)",
+                inline: false
+              },
+              {
+                name: "\u200b",
+                value:"\u200b"
+              }],
+              timestamp: new Date(),
+              footer: {
+                icon_url: "https://cdn.discordapp.com/avatars/883390927383724112/1fdd55ef73a631edfd680964a28ca280.webp",
+                text: "Mini bot says hello!"
+              }
+            }]});
     },
 };
