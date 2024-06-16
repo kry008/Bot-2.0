ca4e9012194f4972425577040fe8ea8ffcc205d7
kry008 <git@mail.kry008.xyz>
2024-06-16 10:36:11 +0200

    repair join fn

--- events/newGuild.js
+++ events/newGuild.js
@@ -1,5 +1,6 @@
 const { Events } = require('discord.js');
 const fs = require('fs');
+const { prefix } = require('../config.json');
 
 module.exports = {
 	name: Events.GuildCreate,
@@ -11,7 +12,7 @@ module.exports = {
         fs.appendFileSync(guildPath, `JOIN: \`${guild.name}\` - ${guild.id} - ${new Date().toLocaleString()}\n`);
         const guildPath2 = `./guilds/guilds-${guild.id}.json`;
 			if (!fs.existsSync(guildPath2)) {
-                fs.writeFileSync(guildPath2, JSON.stringify({ id: guild.id, name: [guild.name], premium: false, prefix: prefix, premium:false, prefix:"^", hello: false, goodbye: true, helloChannel: null, goodbyeChannel: null, rulesChannel: null, welcomeMessage: { title: "Say hello", content: "Hello {{USER}}"}, goodbyeMessage: { title: "Say goodbye", content: "Goodbye {{USER}}"}, rulesMessage: null}));
+                fs.writeFileSync(guildPath2, JSON.stringify({ id: guild.id, name: [guild.name], premium: false, prefix: prefix, premium:false, hello: false, goodbye: true, helloChannel: null, goodbyeChannel: null, rulesChannel: null, welcomeMessage: { title: "Say hello", content: "Hello {{USER}}"}, goodbyeMessage: { title: "Say goodbye", content: "Goodbye {{USER}}"}, rulesMessage: null}));
             }
             else
             {
